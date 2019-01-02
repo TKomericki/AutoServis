333c96c6757a28247e327c7cce49414988dd0d90
fjandel <filip.jandel@fer.hr>
2019-01-02 16:35:47 +0100

    dodan razred RadnoVrijeme

---
+++ AutoServisProject/spring-login-master/src/main/java/com/gpch/login/model/RadnoVrijeme.java
@@ -0,0 +1,154 @@
+package com.gpch.login.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class RadnoVrijeme {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_radnog_vremena")
+	private int idRadnogVremena;
+	
+	@Column(name = "pon_pocetak")
+	@NotEmpty
+	private LocalTime ponedjeljakPocetak;
+	
+	@Column(name = "pon_kraj")
+	@NotEmpty
+	private LocalTime ponedjeljakKraj;
+	
+	
+	@Column(name = "uto_pocetak")
+	@NotEmpty
+	private LocalTime utorakPocetak;
+	
+	@Column(name = "uto_kraj")
+	@NotEmpty
+	private LocalTime utorakKraj;
+	
+	@Column(name = "sri_pocetak")
+	@NotEmpty
+	private LocalTime srijedaPocetak;
+	
+	@Column(name = "sri_kraj")
+	@NotEmpty
+	private LocalTime srijedaKraj;
+	
+	@Column(name = "cet_pocetak")
+	@NotEmpty
+	private LocalTime cetvrtakPocetak;
+	
+	@Column(name = "cet_kraj")
+	@NotEmpty
+	private LocalTime cetvrtakKraj;
+	
+	@Column(name = "pet_pocetak")
+	@NotEmpty
+	private LocalTime petakPocetak;
+	
+	@Column(name = "pet_kraj")
+	@NotEmpty
+	private LocalTime petakKraj;
+
+	public int getIdRadnogVremena() {
+		return idRadnogVremena;
+	}
+
+	public LocalTime getPonedjeljakPocetak() {
+		return ponedjeljakPocetak;
+	}
+
+	public void setPonedjeljakPocetak(LocalTime ponedjeljakPocetak) {
+		this.ponedjeljakPocetak = ponedjeljakPocetak;
+	}
+
+	public LocalTime getPonedjeljakKraj() {
+		return ponedjeljakKraj;
+	}
+
+	public void setPonedjeljakKraj(LocalTime ponedjeljakKraj) {
+		this.ponedjeljakKraj = ponedjeljakKraj;
+	}
+
+	public LocalTime getUtorakPocetak() {
+		return utorakPocetak;
+	}
+
+	public void setUtorakPocetak(LocalTime utorakPocetak) {
+		this.utorakPocetak = utorakPocetak;
+	}
+
+	public LocalTime getUtorakKraj() {
+		return utorakKraj;
+	}
+
+	public void setUtorakKraj(LocalTime utorakKraj) {
+		this.utorakKraj = utorakKraj;
+	}
+
+	public LocalTime getSrijedaPocetak() {
+		return srijedaPocetak;
+	}
+
+	public void setSrijedaPocetak(LocalTime srijedaPocetak) {
+		this.srijedaPocetak = srijedaPocetak;
+	}
+
+	public LocalTime getSrijedaKraj() {
+		return srijedaKraj;
+	}
+
+	public void setSrijedaKraj(LocalTime srijedaKraj) {
+		this.srijedaKraj = srijedaKraj;
+	}
+
+	public LocalTime getCetvrtakPocetak() {
+		return cetvrtakPocetak;
+	}
+
+	public void setCetvrtakPocetak(LocalTime cetvrtakPocetak) {
+		this.cetvrtakPocetak = cetvrtakPocetak;
+	}
+
+	public LocalTime getCetvrtakKraj() {
+		return cetvrtakKraj;
+	}
+
+	public void setCetvrtakKraj(LocalTime cetvrtakKraj) {
+		this.cetvrtakKraj = cetvrtakKraj;
+	}
+
+	public LocalTime getPetakPocetak() {
+		return petakPocetak;
+	}
+
+	public void setPetakPocetak(LocalTime petakPocetak) {
+		this.petakPocetak = petakPocetak;
+	}
+
+	public LocalTime getPetakKraj() {
+		return petakKraj;
+	}
+
+	public void setPetakKraj(LocalTime petakKraj) {
+		this.petakKraj = petakKraj;
+	}
+}
--- AutoServisProject/spring-login-master/src/main/resources/data.sql
+++ AutoServisProject/spring-login-master/src/main/resources/data.sql
@@ -3,3 +3,5 @@ replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (1, 'zamjena gu
 replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (2, 'zamjena ulja', 230);
 replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (3, 'zamjena filtera zraka', 100);
 replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (4, 'zamjena filtera ulja', 100);
+replace into radno_vrijeme(id_radnog_vremena, pon_pocetak, pon_kraj, uto_pocetak, uto_kraj, sri_pocetak, sri_kraj, cet_pocetak, cet_kraj, pet_pocetak, pet_kraj)values (1, '7:00:00', '13:00:00', '12:00:00', '18:00:00', '7:00:00', '13:00:00', '12:00:00', '18:00:00', '7:00:00', '13:00:00');
+replace into radno_vrijeme(id_radnog_vremena, pon_pocetak, pon_kraj, uto_pocetak, uto_kraj, sri_pocetak, sri_kraj, cet_pocetak, cet_kraj, pet_pocetak, pet_kraj)values (2, '12:00:00', '18:00:00', '7:00:00', '13:00:00', '12:00:00', '18:00:00', '7:00:00', '13:00:00', '12:00:00', '18:00:00');
