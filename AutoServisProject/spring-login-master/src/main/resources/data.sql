8e08ddfd5cf6cb39452ea07f621b918c3c95aa60
fjandel <filip.jandel@fer.hr>
2019-01-02 15:44:29 +0100

    dodan razred Usluga i default-ne usluge

---
+++ AutoServisProject/spring-login-master/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+use login;
+replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (1, 'zamjena guma', 200);
+replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (2, 'zamjena ulja', 230);
+replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (3, 'zamjena filtera zraka', 100);
+replace into usluga(id_usluge, ime_usluge, cijena_usluge) values (4, 'zamjena filtera ulja', 100);
